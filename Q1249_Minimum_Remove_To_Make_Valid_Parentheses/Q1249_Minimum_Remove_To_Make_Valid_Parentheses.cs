1a56cc6ddfc91f79c7dc00315fafcf4f8e035383
Bogdan Hladiuc <bogdan.hladiuc@gmail.com>
2022-04-12 15:33:36 -0400

    Q1249 Minimum Remove to Make Valid Parentheses

---
+++ Q1249_Minimum_Remove_To_Make_Valid_Parentheses/Q1249_Minimum_Remove_To_Make_Valid_Parentheses.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Q1249_Minimum_Remove_To_Make_Valid_Parentheses {
+    public class Q1249_Minimum_Remove_To_Make_Valid_Parentheses {
+
+        private static string Solution(string s) {
+            Stack<int> stack = new Stack<int>();
+            HashSet<int> indexesToRemove = new HashSet<int>();
+
+            for (int i = 0; i < s.Length; i++) {
+                if (s[i] == '(') {
+                    stack.Push(i);
+                } else if (s[i] == ')') {
+                    if (stack.Count == 0) {
+                        indexesToRemove.Add(i);
+                    } else {
+                        stack.Pop();
+                    }
+                }
+            }
+
+            while (stack.Count > 0) {
+                indexesToRemove.Add(stack.Pop());
+            }
+
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < s.Length; i++) {
+                if (!indexesToRemove.Contains(i)) {
+                    result.Append(s[i]);
+                }
+            }
+            return result.ToString();
+        }
+
+        public static void Run() {
+            string s = "))((";
+            string result = Solution(s);
+            Console.WriteLine($"Input: {s}");
+            Console.WriteLine($"Output: {result}");
+        }
+    }
+}
--- Run/Program.cs
+++ Run/Program.cs
@@ -3,11 +3,12 @@
 namespace Run {
     class Program {
         static void Main(string[] args) {
-            Q273_Integer_To_English_Words.Q273_Integer_To_English_Words.Run();
+            // Q273_Integer_To_English_Words.Q273_Integer_To_English_Words.Run();
             // Q316_Remove_Duplicate_Letters.Q316_Remove_Duplocate_Letters.Run();
             // Q383_Ransom_Note.Ransom_Note.Run();
             // Q973_K_Closest_Points_To_Origin.Q973_K_Closest_Points_To_Origin.Run();
             // Q986_Interval_List_Intersections.Q986_Interval_List_Intersections.Run();
+            Q1249_Minimum_Remove_To_Make_Valid_Parentheses.Q1249_Minimum_Remove_To_Make_Valid_Parentheses.Run();
         }
     }
 }
