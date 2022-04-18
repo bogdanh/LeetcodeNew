73f4eb3e278d36771be9fdcb8f7d632f476a2a7c
Bogdan Hladiuc <bogdan.hladiuc@gmail.com>
2022-04-17 22:34:47 -0400

    Q282 Expression Add Operators

---
+++ Q282_Expression_Add_Operators/Q282_Expression_Add_Operators.cs
@@ -0,0 +1,78 @@
+﻿using System.Text;
+
+namespace Q282_Expression_Add_Operators {
+
+    public class Q282_Expression_Add_Operators {
+
+        private static IList<string> Solution(string num, int target) {
+            IList<string> result = new List<string>();
+            Helper(result, num, target, 0, 0, 0, new StringBuilder());
+            return result;
+        }
+
+        private static void Helper(
+            IList<string> result,
+            string num,
+            int target,
+            int pos,
+            long currVal,
+            long prevValue,
+            StringBuilder expr) {
+
+            if (pos == num.Length) {
+                if (currVal == target) {
+                    result.Add(expr.ToString());
+                }
+
+                return;
+            }
+
+            for (int i = pos; i < num.Length; i++) {
+                string currNum = num.Substring(pos, i + 1 - pos);
+
+                if (currNum.Length > 1 && currNum[0] == '0') {
+                    break;
+                }
+
+                long currOp = long.Parse(currNum);
+                int len = expr.Length;
+
+                if (pos == 0) {
+                    Helper(result, num, target, i + 1, currOp, currOp, expr.Append(currOp));
+                    expr.Length = len;
+                } else {
+                    Helper(result, num, target, i + 1, currVal + currOp, currOp, expr.Append($"+{currOp}"));
+                    expr.Length = len;
+
+                    Helper(result, num, target, i + 1, currVal - currOp, -currOp, expr.Append($"-{currOp}"));
+                    expr.Length = len;
+
+                    Helper(result, num, target, i + 1, currVal - prevValue + prevValue * currOp, prevValue * currOp, expr.Append($"*{currOp}"));
+                }
+            }
+        }
+
+        public static void Run() {
+            string num = "123";
+            int target = 6;
+
+            Console.WriteLine($"num: {num}");
+            Console.WriteLine($"target: {target}");
+            IList<string> result = Solution(num, target);
+            PrintStringList(result);
+        }
+
+        private static void PrintStringList(IList<string> stringList) {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("[");
+
+            foreach (string str in stringList) {
+                sb.Append($"{str}, ");
+            }
+
+            sb.Length = sb.Length - 2;
+            sb.Append("]");
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
--- Run/Program.cs
+++ Run/Program.cs
@@ -4,13 +4,14 @@ namespace Run {
     class Program {
         static void Main(string[] args) {
             // Q273_Integer_To_English_Words.Q273_Integer_To_English_Words.Run();
+            Q282_Expression_Add_Operators.Q282_Expression_Add_Operators.Run();
             // Q316_Remove_Duplicate_Letters.Q316_Remove_Duplocate_Letters.Run();
             // Q383_Ransom_Note.Ransom_Note.Run();
             // Q621_Task_Scheduler.Q621_Task_Scheduler.Run();
             // Q973_K_Closest_Points_To_Origin.Q973_K_Closest_Points_To_Origin.Run();
             // Q986_Interval_List_Intersections.Q986_Interval_List_Intersections.Run();
             // Q1249_Minimum_Remove_To_Make_Valid_Parentheses.Q1249_Minimum_Remove_To_Make_Valid_Parentheses.Run();
-            Q1428_Leftmost_Column_With_Least_One.Q1428_Leftmost_Column_With_Least_One.Run();
+            // Q1428_Leftmost_Column_With_Least_One.Q1428_Leftmost_Column_With_Least_One.Run();
         }
     }
 }
