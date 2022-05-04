8bd3f562474a184c0f08b79de2e6a5e0843e5267
Bogdan Hladiuc <bogdan.hladiuc@gmail.com>
2022-05-04 15:52:25 -0400

    Q523 Continuous Subarray Sum

---
+++ Q523_Continuous_Subarray_Sum/Q523_Continuous_Subarray_Sum.cs
@@ -0,0 +1,47 @@
+﻿using Library;
+
+namespace Q523_Continuous_Subarray_Sum {
+
+    public class Q523_Continuous_Subarray_Sum {
+        public static void Run() {
+            int[] nums = new int[] { 23, 2, 4, 6, 7 };
+            int k = 6;
+            AssortedMethods.PrintIntArray(nums);
+            Console.WriteLine($"k: {k}");
+            Console.WriteLine(Solution(nums, k));
+        }
+        
+        /*
+        Let's assume d = b - a (b being grater than a). So to have a subarray which sum is multiply of k then we need to find a subarray
+        between a and b which mod % k = 0 meaning d % k = 0.
+        d = b - a
+        d % k = (b - a) % k
+        d % k = b % k - a % k
+        d % k = 0 => 0 = b % k - a % k
+        a % k = b % k
+        So if we calculate runningSum for each element in the nums array. If k != 0 we calculate runningSum % k, if runningSum % k (b % k) 
+        already exists in the dictionary (a % k) then check if there are already 2 elements between these two position is at least 2.
+        If yes return true, otherwise add to the dictionary the runningSum % k as key and index i as value.
+        */
+        private static bool Solution(int[] nums, int k) {
+            Dictionary<int, int> map = new Dictionary<int, int>();
+            map[0] = -1;
+            int runningSum = 0;
+            
+            for (int i = 0; i < nums.Length; i++) {
+                runningSum += nums[i];
+                if (k > 0) {
+                    runningSum %= k;
+                }
+                
+                if (map.ContainsKey(runningSum) && i - map[runningSum] > 1) {
+                    return true;
+                }
+
+                map[runningSum] = i;
+            }
+
+            return false;
+        }
+    }
+}
--- Run/Program.cs
+++ Run/Program.cs
@@ -7,13 +7,14 @@ namespace Run {
             // Q65_Valid_Number.Q65_Valid_Number.Run();
             // Q173_Binary_Search_Tree_Iterator.Q173_Binary_Search_Tree_Iterator.Run();
             // Q215_Kth_Largest_Element.Q215_Kth_Largest_Element.Run();
-            Q236_Lowest_Common_Ancestor_of_a_Binary_Tree.Q236_Lowest_Common_Ancestor_of_a_Binary_Tree.Run();
+            // Q236_Lowest_Common_Ancestor_of_a_Binary_Tree.Q236_Lowest_Common_Ancestor_of_a_Binary_Tree.Run();
             // Q269_Alien_Dictionary.Q269_Alien_Dictionary.Run();
             // Q273_Integer_To_English_Words.Q273_Integer_To_English_Words.Run();
             // Q282_Expression_Add_Operators.Q282_Expression_Add_Operators.Run();
             // Q297_Serialize_Deserialize_Binary_Tree.Q297_Serialize_Deserialize_Binary_Tree.Run();
             // Q316_Remove_Duplicate_Letters.Q316_Remove_Duplocate_Letters.Run();
             // Q383_Ransom_Note.Ransom_Note.Run();
+            Q523_Continuous_Subarray_Sum.Q523_Continuous_Subarray_Sum.Run();
             // Q528_Random_Pick_with_Weight.Q528_Random_Pick_with_Weight.Run();
             // Q621_Task_Scheduler.Q621_Task_Scheduler.Run();
             // Q689_Maximum_Sum_of_3_Non_Overlapping_Subarrays.Q689_Maximum_Sum_of_3_Non_Overlapping_Subarrays.Run();
