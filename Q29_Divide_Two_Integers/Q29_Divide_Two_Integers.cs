42db2addf3e6843c2469d6236c8810b8ec52166a
Bogdan Hladiuc <bogdan.hladiuc@gmail.com>
2022-05-27 21:51:13 -0400

    Q29 Divide two integers

---
+++ Q29_Divide_Two_Integers/Q29_Divide_Two_Integers.cs
@@ -0,0 +1,49 @@
+﻿namespace Q29_Divide_Two_Integers {
+    public class Q29_Divide_Two_Integers {
+        private static readonly int INT_MIN_HALF = -1073741824;
+        public static void Run() {
+            int dividend = -694;
+            int divisor = -53;
+            int quotient = Solution(dividend, divisor);
+            Console.WriteLine($"{dividend} / {divisor} = {quotient}");
+        }
+
+        private static int Solution(int dividend, int divisor) {
+            if (dividend == int.MinValue && divisor == -1) {
+                return int.MaxValue;
+            }
+            
+            int negatives = 2;
+            int quotient = 0;
+            
+            if (dividend > 0) {
+                negatives--;
+                dividend = -dividend;
+            }
+            
+            if (divisor > 0) {
+                negatives--;
+                divisor = -divisor;
+            }
+            
+            while (divisor >= dividend) {
+                int powerOfTwo = -1;
+                int value = divisor;
+                
+                while (value >= INT_MIN_HALF && value + value >= dividend) {
+                    powerOfTwo += powerOfTwo;
+                    value += value;
+                }
+
+                quotient += powerOfTwo;
+                dividend -= value;
+            }
+            
+            if (negatives != -1) {
+                return -quotient;
+            }
+
+            return quotient;
+        }
+    }
+}
--- Run/Program.cs
+++ Run/Program.cs
@@ -3,7 +3,8 @@
 namespace Run {
     class Program {
         static void Main(string[] args) {
-            Q10_Regular_Expression_Matching.Q10_Regular_Expression_Matching.Run();
+            // Q10_Regular_Expression_Matching.Q10_Regular_Expression_Matching.Run();
+            Q29_Divide_Two_Integers.Q29_Divide_Two_Integers.Run();
             // Q56_Merge_Intervals.Q56_Merge_Intervals.Run();
             // Q65_Valid_Number.Q65_Valid_Number.Run();
             // Q173_Binary_Search_Tree_Iterator.Q173_Binary_Search_Tree_Iterator.Run();
